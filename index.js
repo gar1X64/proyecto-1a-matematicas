a095e3330c6ad02a64779b756aefaa9554cecba2
gar1x64 <pggarcc@gmail.com>
2021-06-12 14:34:02 +0200

    Funcionalidad para validar numeros OK

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
    * @returns
    */
   suma: function (n1, n2) {
-    return n1 + n2;
+    return this.esNumero((n1, n2) ? n1 + n2 : this.mensajeError());
   },
   /**
    * @example
@@ -17,7 +17,7 @@ module.exports = {
    * @returns
    */
   resta: function (n1, n2) {
-    return n1 - n2;
+    return this.esNumero((n1, n2) ? n1 - n2 : this.mensajeError());
   },
   /**
    * @example
@@ -27,7 +27,7 @@ module.exports = {
    * @returns
    */
   multiplicacion: function (n1, n2) {
-    return n1 * n2;
+    return this.esNumero((n1, n2) ? n1 * n2 : this.mensajeError());
   },
   /**
    * @example
@@ -37,6 +37,18 @@ module.exports = {
    * @returns
    */
   division: function (n1, n2) {
-    return n1 / n2;
+    return this.esNumero((n1, n2) ? n1 / n2 : this.mensajeError());
+  },
+  /**
+   * Mensaje de error que se ejecuta cuando no tengamos valores numéricos
+   */
+  mensajeError: function () {
+    console.log("un valor o los valores no son numericos");
+  },
+  esNumero: function (n1, n2) {
+    if (typeof n1 !== "number" || typeof n2 !== "number") {
+      return false;
+    }
+    return false;
   },
 };
